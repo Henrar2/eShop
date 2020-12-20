3655b8a65fba87c5852ded58fed6153fb6007378
Henrar2 <67432021+Henrar2@users.noreply.github.com>
2020-12-20 19:46:00 +0200

    PHP Form Validation and Error Message

--- front/login.php
+++ front/login.php
@@ -1,7 +1,10 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>eShop-Login</title>
+        <title>Login to eShop</title>
             <!-- Responsive width -->
             <meta name="viewport" content="width=device-width, initial-scale=1">
     
@@ -15,8 +18,6 @@
             <!-- My Stylesheet -->
             <link rel="stylesheet" href="style.css">
             
-            <!-- Scripts -->
-            <script src="./loginform.js"></script>
     
     </head>
     <header>
@@ -39,19 +40,46 @@
             </ul>
         </nav> 
     </header>
-    <main>
+    <main style="margin:1rem 0;">
         <body>
-            <div class="login-container">
-                <div class="login-box">
+        <?php
+            
+            function strip_post($field){
+                if(!isset($_POST[$field])) {
+                    $_POST[$field]= '';
+                }else{
+                    $_POST[$field] = stripslashes($_POST[$field]);
+                    $_POST[$field] = htmlspecialchars($_POST[$field]);
+                    $_POST[$field] = trim($_POST[$field]);
+                }
+                return $_POST[$field];
+            } 
+
+            $errors = [];
+            $message = '';
+
+            if(isset($_POST['username']) && strlen(strip_post('username'))==0)$errors['username'] = "Username cannot be empty";
+            else if(isset($_POST['username']) &&  strlen(strip_post('username'))<4) $errors['username'] = "Username must be at least 4 characters";
+            if(isset($_POST['password']) && strlen(strip_post('password'))==0) $errors['password'] = "Password cannot be empty!";
+            else if(isset($_POST['password']) && strlen(strip_post('password'))<4) $errors['password'] = "Password must be at least 4 characters";
+            
+        ?>
+            <div class="login-container" >
+                <form class="login-box" method="POST" action="">
                     <h1>Login</h1>
+                    <p id="message"></p>
                     <label for="username">Username:</label>
-                    <input type="text" id="username" name="username" placeholder="Username" required  onfocusout="validateuser();"></input>
-                    <p id="userinfo" class="info" style="visibility:hidden;">a</p>
+                    <input type="text" id="username" name="username" placeholder="Username"></input>
+                    <?php if(isset($errors['username'])): ?>
+                    <p id="userinfo" class="info"><?php echo $errors['username']?></p>
+                    <?php endif?>
                     <label for="password">Password:</label>
-                    <input type="password" id="password" name="password" placeholder="Password" required  onfocusout="validatepass();"></input>
-                    <p id="passwordinfo" class="info" style="color:red;visibility:hidden;">a<p>
-                    <button id="login" class="btn-primary" type="submit" >Login</button>
-                    </div>
+                    <input type="text" id="password" name="password" placeholder="Password"></input>
+                    <?php if(isset($errors['password'])): ?>
+                    <p id="passwordinfo" class="info"><?php echo $errors['password']?><p>
+                    <?php endif?>
+                    <button id="login" class="btn-primary" type="submit">Login</button>
+                </form>
             </div>
         </body>
     </main>
