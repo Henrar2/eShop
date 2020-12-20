41e9484a56f39c0cc22a1d5297c3773b32c8334b
Henrar2 <67432021+Henrar2@users.noreply.github.com>
2020-12-20 19:51:39 +0200

    Database connect and Redirect on index if error

--- front/config.php
+++ front/config.php
@@ -3,4 +3,8 @@ $host = "localhost"; /* Host name */
 $user = "root"; /* User */
 $password = ""; /* Password */
 $dbname = "eshop"; /* Database name */
+$con =  mysqli_connect($host,$name,$pass,$dbname);
+if(!$con){
+    die(header('Location: http://192.168.1.6/')); //TODO Change IP to an error page 
+}
 ?>
