e57c0df97fbc0e058a64331c4e54b2ec72e42ebe
Henrar2 <67432021+Henrar2@users.noreply.github.com>
2020-12-21 16:31:35 +0200

    signin script for signin.php page

---
+++ front/include/signin.inc.php
@@ -0,0 +1,45 @@
+<?php
+if(isset($_POST['login'])){
+    require './config.php';
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if( empty($username) || empty($password) ){
+        header("Location:../signin.php?error=emptyfields");
+        exit();
+    }else{
+        $sql = "SELECT * FROM user WHERE username=? OR mail=?";
+        $stmt = mysqli_stmt_init($con);
+        if(!mysqli_stmt_prepare($stmt,$sql)){
+            header("Location:../signin.php?error=sqlerror");
+            exit();
+        }
+        else{
+
+            mysqli_stmt_bind_param($stmt,"ss",$username,$username);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            if($row=mysqli_fetch_assoc($result)){
+                $passwordCheck = password_verify($password,$row['passcode']);
+                //Check if passwordCheck == false;
+                if(!$passwordCheck){
+                    header("Location:../signin.php?error=wrongpassword");
+                    exit();
+                }
+                else{
+                    session_start();
+                    $_SESSION['userid'] = $row['id'];
+                    $_SESSION['username'] = $row['username'];
+                    header("Location:../dashboard/");
+                }
+            }else{
+                header("Location: ../signin.php?error=nouser");
+                exit();
+            }
+        }
+    }
+}else{
+    header("Location: ../signin.php");
+    exit();
+}
+?>
