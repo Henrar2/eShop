92bbf41e7061cf1b6dda447cab8eefadea4805db
Henrar2 <67432021+Henrar2@users.noreply.github.com>
2020-12-21 16:22:39 +0200

    Config relocate and redirect
    Moved to include folder
    Redirects the user to home page

---
+++ front/include/config.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost"; /* Host name */
+$name = "root";    /* User */
+$pass = "";        /* Password */
+$dbname = "eshop";     /* Database name */
+$con =  mysqli_connect($host,$name,$pass,$dbname);
+if(!$con){
+    die(header('Location: ./index.php')); //TODO Change IP to an error page 
+}else{
+    header('Location: ../index.php?error=authorizedNO');
+}
+?>
