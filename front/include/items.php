beb3317ec1a85bd9f74a37b54f18bc5a55ce0de9
Henrar2 <67432021+Henrar2@users.noreply.github.com>
2020-12-23 12:35:54 +0200

    Dynamic load of items from database

---
+++ front/include/items.php
@@ -0,0 +1,25 @@
+<?php
+    require 'config.php';
+    $sql = "SELECT * FROM item WHERE 1;";
+    $stmt = mysqli_stmt_init($con);
+    if(!mysqli_stmt_prepare($stmt,$sql)){
+        echo "Could not load items";
+    }
+    else{
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        if($result->num_rows>0){
+            while($row=mysqli_fetch_assoc($result)): ?>
+                <div class="card">
+
+                    <img class="card-img-top" src="<?php echo $row['imagepng']?>" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $row['name'] ?></h5>
+                        <p class="card-text"><?php echo $row['description'] ?></p>
+                        <br>
+                        <a href="#footer" class="btn btn-dark" >Προσθήκη στο καλάθι</a>
+                    </div>
+                </div>
+            <?php endwhile?>
+<?php }}
+?>
