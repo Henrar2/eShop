27e2ea3d2f1c1deb0713d75d46e590e1bf31e970
Henrar2 <67432021+Henrar2@users.noreply.github.com>
2020-12-24 15:33:37 +0200

    Added error messages display

--- front/signin.php
+++ front/signin.php
@@ -23,26 +23,38 @@ if(isset($_SESSION['userid'])){
             
     
     </head>
-    <header>
+    <?php 
+    if(isset($_GET['error'])){
+        if($_GET['error']=='emptyfields'){
+            $errors = "Please fill all the fields";
+        }elseif($_GET['error']=='sqlerror'){
+            $errors = "You could not proccess you request.\nTry again later";
+        }elseif($_GET['error']=='wrongpassword'){
+            $errors = "Username or Password is wrong";
+        }elseif($_GET['error']=='nouser'){
+            $errors = "No such user";
+        }
+    }
+    ?>
         <!-- Header -->
         <?php include ("./components/header.php")?>
-    </header>
-    <main style="margin:1rem 0;">
-        <body>
-            <div class="login-container" >
-                <form class="login-box" method="POST" action="./include/signin.inc.php">
-                    <h1>Login</h1>
-                    <p id="message" class="info" style="color:rgb(173, 41, 41)";><?php if(isset($errors)) echo $errors;?></p>
-                    <label for="username">Username or E-mail:</label>
-                    <input type="text" id="username" name="username" placeholder="Username or E-mail"></input>
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password" placeholder="Password"></input>
-                    <button id="login" class="btn-primary" type="submit" name="login">Login</button>
-                    <a href="signup.php" id="signup" name="signup">Sign Up</a>
-                    <a href="reset.php" id="reset" name="reset"style="padding-bottom: 2rem;">Forgot Password?</a>
-                </form>
-            </div>
-        </body>
-    </main>
-    <?php include ("./components/footer.php"); ?>
+        <main style="margin:1rem 0;">
+            <body>
+                <div class="login-container" >
+                    <form class="login-box" method="POST" action="./include/signin.inc.php">
+                        <h1>Login</h1>
+                        <!-- TODO find a way to change this ugly thing below -->
+                        <p id="message" class="info";><?php if(isset($errors)){echo $errors;}else{ ?><script>document.getElementById("message").style.backgroundColor="transparent"</script><?php }?></p>
+                        <label for="username">Username or E-mail:</label>
+                        <input type="text" id="username" name="username" placeholder="Username or E-mail"></input>
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" name="password" placeholder="Password"></input>
+                        <button id="login" class="btn-primary" type="submit" name="login">Login</button>
+                        <a href="signup.php" id="signup" name="signup">Sign Up</a>
+                        <a href="reset.php" id="reset" name="reset"style="padding-bottom: 2rem;">Forgot Password?</a>
+                    </form>
+                </div>
+            </body>
+        </main>
+        <?php include ("./components/footer.php"); ?>
 </html>
--- front/signup.php
+++ front/signup.php
@@ -23,24 +23,45 @@ if(isset($_SESSION['userid'])){
             
     
     </head>
-    <header>
+    <?php 
+    if(isset($_GET['error'])){
+        if($_GET['error']=='emptyfields'){
+            $errors = "Please fill all the fields";
+        }elseif($_GET['error']=='invalidmailandusername'){
+            $errors = "Invalid email and username";
+        }elseif($_GET['error']=='invalidusername'){
+            $errors = "Etner a valid username";
+        }elseif($_GET['error']=='invalidmail'){
+            $errors = "Enter a valid email";
+        }elseif($_GET['error']=='passwordcrit'){
+            $errors = "Enter a valid password";
+        }elseif($_GET['error']=='passwordmatch'){
+            $errors = "Passwords do not match";
+        }elseif($_GET['error']=='usertaken'){
+            $errors = "User already exists";
+        }elseif($_GET['error']=='sqlerror'){
+            $errors = "You could not proccess you request.\nTry again later";
+        }
+    }
+    ?>
         <!-- Header -->
         <?php include ("./components/header.php")?>
-    </header>
+
     <main style="margin:1rem 0;">
         <body>
             <div class="login-container" >
                 <form class="login-box" method="POST" action="include/signup.inc.php">
                     <h1>Sign Up</h1>
-                    <p id="message" style="color:rgb(173, 41, 41)";><?php if(isset($errors)) echo $errors;?></p>
+                    <!-- TODO find a way to change this ugly thing below -->
+                    <p id="message" class="info";><?php if(isset($errors)){echo $errors;}else{ ?><script>document.getElementById("message").style.backgroundColor="transparent"</script><?php }?></p>
                     <label for="username">Username:</label>
-                    <input type="text" id="username" name="username" placeholder="Username"></input>
+                    <input type="text" id="username" name="username" placeholder="Username" value="<?php if(isset($_GET['username'])) echo $_GET['username'];?>"></input>
                     <label for="username">E-mail:</label>
-                    <input type="mail" id="mail" name="mail" placeholder="E-mail"></input>
+                    <input type="mail" id="mail" name="mail" placeholder="E-mail" value="<?php if(isset($_GET['mail'])) echo $_GET['mail'];?>"></input>
                     <label for="password">Password:</label>
                     <input type="password" id="password" name="password" placeholder="Password"></input>
                     <label for="rpasword">Repeat Password:</label>
-                    <input type="text" id="rpassword" name="rpassword" placeholder="Repeat Password"></input>
+                    <input type="password" id="rpassword" name="rpassword" placeholder="Repeat Password"></input>
                     <button id="signup" class="btn-primary" type="submit" name="signup">Sign Up</button>
                     <a href="signin.php">I already have an account</a>
                     <a href="reset.php" style="visibility:hidden;">Forgot Password?</a>
