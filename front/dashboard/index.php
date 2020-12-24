9c0189ea52599fae82d87ac4857cf7d182b9d910
Henrar2 <67432021+Henrar2@users.noreply.github.com>
2020-12-24 15:42:20 +0200

    Dashboard page layout

--- front/dashboard/index.php
+++ front/dashboard/index.php
@@ -24,13 +24,68 @@ session_start();
     
         </head>
         <body>
-            <header>
-                <!-- Header -->
-               <?php include ("../components/header.php"); ?>
-            </header>
-            <main>
+
+        <!-- Header -->
+        <?php include ("../components/header.php"); ?>
+
+            
+    <main style="/* background-color:red; */margin: 1rem 0 1rem 0;display: flex;flex-direction: row;height: 100%;min-height: 80vh;box-shadow: 1px 1px 2px 2px black;">
+     <nav class="dash-nav" style="/* height:100%; */display: flex;/* position: absolute; */float:left;flex-direction: column;gap: 2rem;align-items: center;list-style:none;padding:1rem 1rem 0 1rem;background-color: rosybrown;width: 20%;">
+            <div>
                 <h1>Hello <?php echo $_SESSION['username']; ?></h1>
-                <a href="../include/signout.inc.php">Logout!</a>
+                <a href="../include/signout.inc.php">Logout!</a> 
+            </div>
+                <ul class="list" style=""> 
+                    <ul class="listitem" style=""><a href="#">News</a>
+                        <li style="padding: 8px 1px;">asd</li>
+                        <li style="padding: 8px 1px;">asd</li>
+                        <li style="padding: 8px 1px;">asd</li>
+                        <li style="padding: 8px 1px;">asd</li>
+                    </ul>
+                    <li class="listitem" style="padding: 8px 1px;"><a href="#">Sales</a></li>
+                    <li class="listitem" style="padding: 8px 1px;"><a href="#">Items</a></li>  
+                </ul>
+            </nav>
+    <div class="mainbody" bis_skin_checked="1" style="
+    display: flex;
+    background: black;
+    width: 80%;
+    float: left;
+    margin: 1rem 1rem 1rem 1rem;
+    box-shadow: 0 0 5px 1px black;
+    height: 80vh;
+    -webkit-scrollbar {;
+}
+
+/* Track */
+::
+    -webkit-scrollbar-track {;
+
+}
+ 
+/* Handle */
+::
+    -webkit-scrollbar-thumb {;
+
+
+}
+
+/* Handle on hover */
+::
+    -webkit-scrollbar-thumb:hover {
+  background: #3676e8; 
+};
+">
+<div class="maincontent" bis_skin_checked="1" style="
+    height: 500vh;
+    background: green;
+">
+<ul>
+    <li>asdasdasdasd</li>
+    <li>asdasdasdasd</li>
+    </ul>
+</div></div>
+
             </main>
            <?php include ("../components/footer.php"); ?>
         </body>
