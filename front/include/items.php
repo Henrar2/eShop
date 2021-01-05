8d9ac96a2475caa3a2ecd5fd5564883e69a878d0
Henrar2 <67432021+Henrar2@users.noreply.github.com>
2021-01-05 13:16:49 +0200

    code spacing

--- front/include/items.php
+++ front/include/items.php
@@ -11,13 +11,12 @@
         if($result->num_rows>0){
             while($row=mysqli_fetch_assoc($result)): ?>
                 <div class="card">
-
-                    <img class="card-img-top" src="<?php echo $row['imagepng']?>" alt="Card image cap">
+                    <img class="card-img-top"src="<?php echo $row['imagepng']?>" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title"><?php echo $row['name'] ?></h5>
                         <p class="card-text"><?php echo $row['description'] ?></p>
                         <br>
-                        <a href="#footer" class="btn btn-dark" >Προσθήκη στο καλάθι</a>
+                        <a href="#footer" class="btn btn-dark">Προσθήκη στο καλάθι</a>
                     </div>
                 </div>
             <?php endwhile?>
