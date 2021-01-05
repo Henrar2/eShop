caadd71be26d106ba4644f608dc0d3599096d9b8
Henrar2 <67432021+Henrar2@users.noreply.github.com>
2021-01-05 13:15:16 +0200

    DashNav animation

---
+++ front/dashboard/js/navHandle.js
@@ -0,0 +1,52 @@
+//    ANIMATION IS LAGGY (Revisit or Delete)
+function navHandle(){
+    var el = document.getElementsByClassName("text");
+    var dash = document.getElementById("dashbar");
+    dash.style.transition="400ms";
+    if(dash.style.width=="6rem"){
+        //Hide the items first
+        for(let i=0;i<el.length;i++){
+            el[i].style.transisition = "400ms";
+            el[i].style.display = "none";
+        }
+        // wait 400ms
+        setTimeout(function(){
+            //Expand DashBar 
+            dash.style.width="2.5%";
+        },400);   
+    }else{
+        // Expand DashBar first
+        dash.style.width="6rem";
+        //wait 400ms
+        setTimeout(function(){
+            //Display all items
+            for(let i=0;i<el.length;i++){
+                el[i].style.transisition = "400ms";
+                el[i].style.display = "inline";
+            }
+        },400);
+    }     
+}
+
+// function navHandle(){
+//     document.getElementById("dashbar").style.transition="400ms";
+//     document.getElementById("text1").style.transition="400ms";
+//     document.getElementById("text2").style.transition="400ms";
+//     document.getElementById("text3").style.transition="400ms";
+//     document.getElementById("text4").style.transition="400ms";
+
+//     if(document.getElementById("dashbar").style.width=="6rem"){
+//         document.getElementById("dashbar").style.width="2.5%";
+//         document.getElementById("text1").style.display="none";
+//         document.getElementById("text2").style.display="none";
+//         document.getElementById("text3").style.display="none";
+//         document.getElementById("text4").style.display="none";
+//     }else{
+//         document.getElementById("dashbar").style.transition="400ms";
+//         document.getElementById("dashbar").style.width="6rem";
+//         document.getElementById("text1").style.display="inline";
+//         document.getElementById("text2").style.display="inline";
+//         document.getElementById("text3").style.display="inline";
+//         document.getElementById("text4").style.display="inline";
+//     }
+// }
