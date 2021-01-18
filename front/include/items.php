978fcfec85face5d82693704854c1c44d85b87a6
Henrar2 <67432021+Henrar2@users.noreply.github.com>
2021-01-18 14:50:43 +0200

    path fix

--- front/include/items.php
+++ front/include/items.php
@@ -11,7 +11,7 @@
         if($result->num_rows>0){
             while($row=mysqli_fetch_assoc($result)): ?>
                 <div class="card">
-                    <img class="card-img-top"src="<?php echo $row['imagepng']?>" alt="Card image cap">
+                    <img class="card-img-top"src="<?php echo "./imgs/".$row['img']?>" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title"><?php echo $row['name'] ?></h5>
                         <p class="card-text"><?php echo $row['description'] ?></p>
