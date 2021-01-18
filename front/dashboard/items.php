87365a4abd3d6143a97378ad760e2a79c48f6283
Henrar2 <67432021+Henrar2@users.noreply.github.com>
2021-01-18 14:42:46 +0200

    renamed additems.php to items.php

--- front/dashboard/items.php
+++ front/dashboard/items.php
@@ -29,37 +29,54 @@ session_start();
             <script src="./js/navHandle.js"></script>
         </head>
         <body>
-
+        <?php
+             if(isset($_GET['error']) && $_GET['error']=='emptyfields'){
+                $errors = "Please fill all the fields";
+             }
+        ?>
         <!-- Header -->
         <?php include ("../components/header.php"); ?>
 
             
-    <main style="margin: 1rem 0 1rem 0;display: flex;flex-direction: row;height: 100%;min-height: 80vh;box-shadow: 1px 1px 2px 2px black;">
+    <main style="margin: 1rem 0 1rem 0;display: flex;flex-direction: row;height: 100%;width:100%;min-height: 80vh;box-shadow: 1px 1px 2px 2px black;">
         <?php include ("../components/dashnav.php"); ?>
+
+    <!-- POST ITEMS FORM -->
     <div class="mainbody" bis_skin_checked="1">
         <h1 style="text-align:center;">Add an Item</h1><br>
-        <form class="items-form" method="POST" action="./include/items.inc.php">
+        <p id="message" class="info";><?php if(isset($errors)){echo $errors;}else{ ?><script>document.getElementById("message").style.backgroundColor="transparent"</script><?php }?></p>
+        <form class="items-form" method="POST" action="./include/items.inc.php" enctype="multipart/form-data">
             <span class="col">
                 <label for="item-name">Name</label>
-                <input type="text" name="item-name" class="item-name" id="item-name" placeholder="Name"></input>
+                <input type="text" name="item-name" class="input" id="item-name" placeholder="Name" value="<?php if(isset($_GET['name'])) echo $_GET['name']?>"></input>
             </span>
             <span class="col">
                 <label for="item-desc">Description</label>
-                <input type="text" name="item-desc" class="item-desc" id="item-desc" placeholder="Description" style="width:20rem;"></input>
+                <input type="text" name="item-desc" class="input" id="item-desc" placeholder="Description" value="<?php if(isset($_GET['desc'])) echo $_GET['desc']?>" ></input>
             </span>
             <span class="col">
                 <label for="item-price">Price</label>
-                <input type="number" name="item-price" class="item-price" id="item-price" placeholder="Price"></input>
+                <input type="number" name="item-price" class="input" id="item-price" placeholder="Price" value="<?php if(isset($_GET['price'])) echo $_GET['price']?>" step=0.01></input>
+            </span>
+            <span class="col">
+                <label for="item-quantity">Quantity</label>
+                <input type="number" name="item-quantity" class="input" id="item-quantity" placeholder="Quantity" value="<?php if(isset($_GET['quantity'])) echo $_GET['quantity']?>" ></input>
+            </span>
+            <span class="col">
+                <label for="fileToUpload">Image</label>
+                <input type="file" name="fileToUpload" class="input" id="fileToUpload" value="<?php if(isset($_GET['img'])) echo $_GET['img']?>">
             </span>
             <span class="col">
                 <label for="submit-item" style="visibility:hidden">Confirm</label>
                 <button type="submit" id="submit-item" class="btn-primary" name="submit-item">Add Item</button>
             </span>
         </form>
-        <table style="background:gray;">
+        <!-- Find and display all items from db -->
+        <div class="tablecontainer" style="overflow:auto;height:20rem;margin:2rem auto 2rem auto;">
+        <table class="utable" style="margin:2rem auto 2rem auto;overflow-y:auto;">
             <ul>
                 <tr>
-                    <th>ID</th><th>Name</th><th>Description</th><th>Price</th><th>Image</th><th>Modify/Delete</th>
+                    <th>ID</th><th>Name</th><th>Description</th><th>Price</th><th>Quantity</th><th>Image</th><th>Modify/Delete</th>
                 </tr>
                     <?php
                         require ("./include/config.php");
@@ -73,20 +90,24 @@ session_start();
                             $result = mysqli_stmt_get_result($stmt);
                             if($result->num_rows>0){
                                 while($row=mysqli_fetch_assoc($result)): ?>
-                            <tr><td><?php echo $row['id']?></td><td><?php echo $row['name']?></td><td><?php echo $row['description']?></td><td><?php echo $row['price']?></td><td><img style="width:2rem;height:2rem;"src="<?php echo $row['imagepng']?>"></img></td><td><span><button class="btn-danger" type="button">Modify</button><button class="btn-info" type="button">Delete</button></span></td></tr>
+                            <tr><td><?php echo $row['id']?></td><td><?php echo $row['name']?></td><td><?php echo $row['description']?></td><td><?php echo $row['price']?></td><td><?php echo $row['quantity']?></td><td><img style="width:100%;"src="<?php echo $row['imagepng']?>"></img></td><td><span><button class="btn-danger" type="button">Modify</button><a href="./include/deleteitem.inc.php?id=<?php echo $row['id']?>" class="btn-info" type="button" name="deletebtn">Delete</button></span></td></tr>
                         <?php endwhile?>
                         <?php 
                             }
                         }?>
 
             </ul>
-        </table> 
+        </table>            
+        </div>
     </div>
             </main>
            <?php include ("../components/footer.php"); ?>
         </body>
     </html>
-    <?php }else{
-
+    <?php 
     }
+    // TERMINATE CONNECTION
+    
+    
+    
     ?>
